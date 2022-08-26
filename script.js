6664dfa6c762250a4bfec4ce47ef7b38636e3154
Mohammad Abid <68217349+abidzmohammad@users.noreply.github.com>
2022-08-26 12:04:32 +0600

    impliment highscore

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
    const guess = Number(document.querySelector(".guess").value);
@@ -13,6 +14,10 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector('.number').style.width = '30rem';
       document.querySelector('.number').style.backgroundColor = '#333';
       document.querySelector('.number').style.color = '#fff';
+
+      if(score > highScore){
+         highScore = score;
+      }
    } else if (guess > secretNumber) {
       if (score > 1) {
          document.querySelector(".message").textContent = "too high try again";
