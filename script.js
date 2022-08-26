099079ef9047a70518e8a0670783485f93b7bdb3
Mohammad Abid <68217349+abidzmohammad@users.noreply.github.com>
2022-08-26 16:19:41 +0600

    final making code dry

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+function displayMessage(message){
+   document.querySelector('.message').textContent = message;
+}
+
 document.querySelector(".check").addEventListener("click", function () {
    const guess = Number(document.querySelector(".guess").value);
    if (!guess) {
-      document.querySelector(".message").textContent = "No Number ⛔";
+      displayMessage("No Number ⛔");
    } else if (guess === secretNumber) {
       document.querySelector(".number").textContent = secretNumber;
-      document.querySelector(".message").textContent = "Correct Number 👌";
+      displayMessage("Correct Number 👌");
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
       document.querySelector('.number').style.backgroundColor = '#f4f4f4';
@@ -21,12 +25,12 @@ document.querySelector(".check").addEventListener("click", function () {
       }
    } else if(guess !== secretNumber){
       if (score > 1) {
-         document.querySelector(".message").textContent = guess > secretNumber ? "too high 📈" : "too low 📉";
+         displayMessage(guess > secretNumber ? "too high 📈" : "too low 📉");
          score--;
          document.querySelector(".score").textContent = score;
       }else{
          document.querySelector(".score").textContent = 0;
-         document.querySelector(".message").textContent = 'you lost the game!';
+         displayMessage('you lost the game!');
       }
    }
 });
@@ -41,7 +45,7 @@ document.querySelector('.again').addEventListener('click', function(){
    document.querySelector('.number').textContent = '?';
    document.querySelector('.number').style.width = '15rem';
    document.querySelector(".guess").value = '';
-   document.querySelector(".message").textContent = 'Start guessing...';
+   displayMessage('Start guessing...');
    score = 20;
    document.querySelector('.score').textContent = score;
 });
